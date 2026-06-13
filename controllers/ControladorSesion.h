9affe86d8d16fbdce65d4d5e598ca99c6611cdf9
Ivan Rodriguez <ivanrms371@192.168.1.132>
2026-06-13 12:31:04 -0300

    ControladorSesion Base

---
+++ controllers/ControladorSesion.h
@@ -0,0 +1,11 @@
+#ifndef CONTROLADORSESION_H
+#define CONTROLADORSESION_H
+
+#include "../interfaces/controllers/IControladorSesion.h"
+
+class ControladorSesion : public IControladorSesion
+{
+public:
+};
+
+#endif
