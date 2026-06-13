de04ddb5cd193df6c2eb5958c52784fe6aa832d5
Ivan Rodriguez <ivanrms371@192.168.1.132>
2026-06-13 12:26:43 -0300

    Subiendo la base de ControladorConsultarPrestamo

---
+++ controllers/ControladorConsultarPrestamo.h
@@ -0,0 +1,11 @@
+#ifndef CONTROLADORCONSULTARPRESTAMO_H
+#define CONTROLADORCONSULTARPRESTAMO_H
+
+#include "../interfaces/controllers/IControladorConsultarPrestamo.h"
+
+class ControladorConsultarPrestamo : public IControladorConsultarPrestamo
+{
+public:
+};
+
+#endif
