5744b95f1432f651839520ac7708822ce4f0cd7b
Gastón Etcheverre <Tonga2562@gmail.com>
2026-06-16 14:37:42 -0300

    main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,41 @@
 #include "menu/menu.h"
 #include "cargarDatos.h"
 
+#include "factory/Factory.h"
+#include "IControladorRegistrarUsuario.h"
+#include "IControladorRegistrarMaterial.h"
+#include "IControladorRegistrarPrestamo.h"
+#include "IControladorVerInfoMaterial.h"
+#include "IControladorSesion.h"
+#include "IControladorConsultarPrestamo.h"
+#include "Sesion.h"
+
+
+Sesion * s;
+Factory * f;
+IControladorRegistrarUsuario *icru;
+IControladorRegistrarMaterial *icrm;
+IControladorRegistrarPrestamo *icrp;
+IControladorVerInfoMaterial *icvim;
+IControladorSesion *ics;
+IControladorConsultarPrestamo *iccp;
+
+
+
+
+
+
 int main()
 {
+    s = Sesion::getInstance();
+    f = Factory::getInstancia();
+    icru = f->getIControladorRegistrarUsuario();
+    icrm = f->getIControladorRegistrarMaterial();
+    icrp = f->getIControladorRegistrarPrestamo();
+    icvim = f->getIControladorVerInfoMaterial();
+    ics = f->getIControladorSesion();
+    iccp = f->getIControladorConsultarPrestamo();
+
     cargarDatos();
     menuAutenticacion();
     return 0;
